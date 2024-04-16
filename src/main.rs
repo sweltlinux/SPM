0334cb9134e0a2d1fc65d8bef7445a057732cb66
Bhraedain Teitgen <Teitgenbhraedain@gmail.com>
2024-04-16 09:27:38 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: install,
+
+    #[arg(short, long)]
+    name: update,
+
+    #[arg(short, long)]
+    name: upgrade,
+
+    #[arg(short, long)]
+    name: search,
+
+    #[arg(short, long)]
+    name: remove
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
